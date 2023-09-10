013ceeb38a68aab93844adab716b4b4aff2feab4
kbrsrkl1 <kubranur216@gmail.com>
2023-09-10 15:41:59 +0300

    commit

---
+++ social-media-app/src/index.js
@@ -0,0 +1,15 @@
+
+
+import App from './App';
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <div>
+    <App />
+  </div>
+);
+
+
